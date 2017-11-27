61bd0c4cc17b0f98b3156313239d977674fdfbdf
Joe Gross <jgross@stimpy.net>
2017-11-26 21:36:13 -0800

    move posting to its own function

--- prometheus-pmsensor.py
+++ prometheus-pmsensor.py
@@ -25,6 +25,20 @@ class sensor_server(object):
         self.co2_ppm = Gauge('co2_ppm', 'CO2 concentration in PPM', ['serial_port'])
         self.co2_temp = Gauge('co2_temp', 'CO2 sensor temp in C', ['serial_port'])
 
+    def post_data(self, res):
+        ppm, temp = res
+        logging.info("CO2: %s ppm, temp: %s C", ppm, temp)
+        # do not post preheat values
+        if ppm in CO2_PREHAT_VALUES:
+            logging.warning("CO2 preheating: Not posting.")
+            return
+        # pmsensor does not checksum
+        if ppm > CO2_MAX_PPM:
+            logging.warning("CO2 over max: Not posting.")
+            return
+        self.co2_ppm.labels(serial_port=self.serial_port).set(ppm)
+        self.co2_temp.labels(serial_port=self.serial_port).set(temp)
+
     def serve_forever(self):
         while True:
             res = None
@@ -33,17 +47,7 @@ class sensor_server(object):
             except (serial.serialutil.SerialException, termios.error) as e:
                 logging.warning(e)
             if res:
-                ppm, temp = res
-                logging.info("CO2: %s ppm, temp: %s C", ppm, temp)
-                # do not post preheat values
-                if ppm in CO2_PREHAT_VALUES:
-                    logging.warning("CO2 preheating: Not posting.")
-                    continue
-                # pmsensor does not checksum
-                if ppm > CO2_MAX_PPM:
-                    logging.warning("CO2 over max: Not posting.")
-                self.co2_ppm.labels(serial_port=self.serial_port).set(ppm)
-                self.co2_temp.labels(serial_port=self.serial_port).set(temp)
+                self.post_data(res)
             logging.debug("sleeping %s...", self.sleep)
             time.sleep(self.sleep)
 
